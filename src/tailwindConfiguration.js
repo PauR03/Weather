b25fdd272df7fc08b7e41c286ce9ab93f4869595
PauR03 <priusnori03@gmail.com>
2024-05-23 18:15:16 +0200

    Continuamos con el frontend

---
+++ src/tailwindConfiguration.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                iSans: ['Inria Sans', 'sans-serif']
+            },
+            colors: {
+                bodyBg: '#D6D7DA',
+                xd: "#F6F6F8"
+            },
+            borderRadius: {
+                '6xl': '3rem'
+            },
+            backgroundImage: {
+                'city': "url('/public/city.webp')"
+            }
+        }
+    }
+}
